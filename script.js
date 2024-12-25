2458045654f7a68acdc379c7d8fc2d27883c4e06
Srihith-Reddy-Nethi <srihithrn@gmail.com>
2024-12-25 19:15:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ search.addEventListener("click", async ()=>{
 
     try{
 
-        const response = await fetch(`http://www.omdbapi.com/?apikey=99328bbd&t=${encodeURIComponent(title)}`);
+        const response = await fetch(`https://www.omdbapi.com/?apikey=99328bbd&t=${encodeURIComponent(title)}`);
         const data = await response.json();
 
         if(data.response === "False"){
